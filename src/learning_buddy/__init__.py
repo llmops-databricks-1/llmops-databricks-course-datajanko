a1bafd4f890ae704c25705b70132e76b9d8972c9
datajanko <jan.koch@tu-dortmund.de>
2026-04-14 23:03:21 +0200

    fixes test

--- src/learning_buddy/__init__.py
+++ src/learning_buddy/__init__.py
@@ -1,8 +1,3 @@
 """Learning Buddy - LLM-powered homework assistance system."""
 
-from learning_buddy.agent import LearningBuddyAgent
-from learning_buddy.config import LearningBuddyProjectConfig
-
 __version__ = "0.0.1"
-
-__all__ = ["LearningBuddyAgent", "LearningBuddyProjectConfig"]
